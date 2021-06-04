df45c0e23def25abf8c1689c9b8f60512fddb2fb
LouisLec <louis.l@obole-digitale.fr>
2021-06-04 16:18:33 +0200

    ✨ install storybook
    NOTA: postCSS addon config in .storybook/main.js
    NOTA: nextjs Image component
    NOTA: nextjs router

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+    "storybook-addon-next-router",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ],
+};
---
+++ .storybook/theme.js
@@ -0,0 +1,11 @@
+import { create } from "@storybook/theming";
+import logo from "../public/images/logo-lg-dark.svg";
+
+export default create({
+  base: "dark",
+  brandTitle: "carambole",
+  brandUrl: "https://carambole.io",
+  brandImage: logo,
+  colorPrimary: "#24294B",
+  colorSecondary: "#7282ED",
+});
